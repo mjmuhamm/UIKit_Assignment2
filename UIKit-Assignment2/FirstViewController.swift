916dbf3dd8e53a4329e3176ddea90f580cb8b4f6
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-03-19 20:01:55 -0400

    additions

---
+++ UIKit-Assignment2/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NextViewController.swift
+//  UIKit-Assignment2
+//
+//  Created by Malik Muhammad on 3/19/26.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
